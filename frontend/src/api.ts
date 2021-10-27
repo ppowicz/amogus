045d64f6420b77d507e95ed01e8c8ebaf3136cc3
Patryk Popowicz <ptrykppowicz@gmail.com>
2021-10-27 18:37:36 +0200

    update provider buttons handling

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -7,34 +7,49 @@ const apiPaths = {
   register: "https://pizzeria.cubepotato.eu/auth/register",
   logout: "https://pizzeria.cubepotato.eu/auth/logout",
   me: "https://pizzeria.cubepotato.eu/auth/me",
-  providers: "https://pizzeria.cubepotato.eu/auth/providers"
-}
+  providers: "https://pizzeria.cubepotato.eu/auth/providers",
+};
 
 const getMenu = async () => {
   let data;
-  await axios.get(apiPaths.menu).then(response => {
-    data = response.data;
-  }).catch(error => console.warn(error));
+  await axios
+    .get(apiPaths.menu)
+    .then((response) => {
+      data = response.data;
+    })
+    .catch((error) => console.warn(error));
   return data;
-}
+};
 
 const login = (email: string, password: string) => {
-  axios.post(apiPaths.login, {email, password}).then(() => window.location.reload).catch(error => console.warn(error));
-}
+  axios
+    .post(apiPaths.login, { email, password })
+    .then(() => window.location.reload)
+    .catch((error) => console.warn(error));
+};
 
 const register = (email: string, password: string) => {
-  axios.post(apiPaths.register, {email, password}).then(() => window.location.reload).catch(error => console.warn(error));
-}
+  axios
+    .post(apiPaths.register, { email, password })
+    .then(() => window.location.reload)
+    .catch((error) => console.warn(error));
+};
 
 const logout = () => {
-  axios.get(apiPaths.logout).then(() => window.location.reload).catch(error => console.warn(error));
-}
+  axios
+    .get(apiPaths.logout)
+    .then(() => window.location.reload)
+    .catch((error) => console.warn(error));
+};
 
 const getCurrentUser = () => {
-  return new Promise<IUser>(resolve => {
-    axios.get(apiPaths.me).then(response => resolve(response.data)).catch(error => console.warn(error));
-  })
-}
+  return new Promise<IUser>((resolve) => {
+    axios
+      .get(apiPaths.me)
+      .then((response) => resolve(response.data))
+      .catch((error) => console.warn(error));
+  });
+};
 
 // const getProviders = () => {
 //   return new Promise(resolve => {
@@ -42,8 +57,16 @@ const getCurrentUser = () => {
 //   })
 // }
 
-const callProvider = (provider:string) => {
-  axios.get(apiPaths.login, {params:{provider}})
-}
+const callProvider = (provider: string) => {
+  document.location.href = apiPaths.login + "?provider=" + provider;
+};
 
-export { apiPaths, getMenu, login, register, logout, getCurrentUser, callProvider }
+export {
+  apiPaths,
+  getMenu,
+  login,
+  register,
+  logout,
+  getCurrentUser,
+  callProvider,
+};
